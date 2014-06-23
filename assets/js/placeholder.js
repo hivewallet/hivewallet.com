63e873353b6e7bcf952bc3f560a56c01e128f778
Ben Jennings <ben@measured.com.au>
2014-06-23 13:39:30 +1000

    Add scrollto links

--- assets/js/placeholder.js
+++ assets/js/placeholder.js
@@ -1,1 +1,14 @@
-// temp js file for when we need it.
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
